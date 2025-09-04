86864bd9f69c518d19112d11c3a20ca4dfd3749a
Zeyad <farghlyzeyad9@gmail.com>
2025-09-04 14:02:09 +0300

    basic auth using authrisation header

--- src/main/java/com/mycompany/app/controller/AuthController.java
+++ src/main/java/com/mycompany/app/controller/AuthController.java
@@ -3,9 +3,11 @@ package com.mycompany.app.controller;
 import com.mycompany.app.dto.LoginRequestDTO;
 import com.mycompany.app.dto.LoginResponseDTO;
 import com.mycompany.app.serivce.AuthService;
-import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,7 +20,29 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> authenticateUser(@Valid @RequestBody LoginRequestDTO loginRequest) {
+    public ResponseEntity<LoginResponseDTO> authenticateUser(
+            @RequestHeader("Authorization") String authHeader) {
+
+        if (authHeader == null || !authHeader.toLowerCase().startsWith("basic ")) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        String base64Credentials = authHeader.substring(6);
+        byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+        String credentials = new String(credDecoded);
+
+        final String[] values = credentials.split(":", 2);
+        if (values.length != 2) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        String email = values[0];
+        String password = values[1];
+
+        LoginRequestDTO loginRequest = new LoginRequestDTO();
+        loginRequest.setEmail(email);
+        loginRequest.setPassword(password);
+
         return authService.authenticateUser(loginRequest);
     }
 }
