0b0b3077f4258d5b84babdd0b41730e7121a68a4
Zeyad <farghlyzeyad9@gmail.com>
2025-09-04 14:44:38 +0300

    using lombok

--- src/main/java/com/mycompany/app/dto/LoginRequestDTO.java
+++ src/main/java/com/mycompany/app/dto/LoginRequestDTO.java
@@ -6,6 +6,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class LoginRequestDTO {
 
     @NotBlank(message = "Email cannot be empty")
@@ -17,20 +27,4 @@ public class LoginRequestDTO {
     @Size(min = 8, message = "Password must be at least 8 characters long")
     @NoHtml(message = "Password cannot contain HTML characters")
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/mycompany/app/dto/LoginResponseDTO.java
+++ src/main/java/com/mycompany/app/dto/LoginResponseDTO.java
@@ -1,39 +1,18 @@
 package com.mycompany.app.dto;
 
-public class LoginResponseDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class LoginResponseDTO {
     private Long id;
     private String email;
-    private String roles;
-
-    public LoginResponseDTO(Long id, String email, String roles) {
-        this.id = id;
-        this.email = email;
-        this.roles = roles;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return roles;
-    }
-
-    public void setRoles(String roles) {
-        this.roles = roles;
-    }
+    private String role;
 }
 
