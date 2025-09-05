e7516f3bf0f2c32bc8d2a8e90d26a998213eab2b
Ahmeedsameer <Ahmedsamiralsayed148@gmail.com>
2025-09-06 01:37:06 +0300

    after editing test to include password hashing

--- src/main/java/org/orange/oie/internship2025/assetmanagementsystem/serivce/PasswordMigrationService.java
+++ src/main/java/org/orange/oie/internship2025/assetmanagementsystem/serivce/PasswordMigrationService.java
@@ -1,37 +1,37 @@
-package org.orange.oie.internship2025.assetmanagementsystem.serivce;
+    package org.orange.oie.internship2025.assetmanagementsystem.serivce;
 
 
 
-import org.orange.oie.internship2025.assetmanagementsystem.entity.User;
-import org.orange.oie.internship2025.assetmanagementsystem.reposetries.UserReposetries;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+    import org.orange.oie.internship2025.assetmanagementsystem.entity.User;
+    import org.orange.oie.internship2025.assetmanagementsystem.reposetries.UserReposetries;
+    import org.springframework.security.crypto.password.PasswordEncoder;
+    import org.springframework.stereotype.Service;
 
-@Service
-public class PasswordMigrationService {
+    @Service
+    public class PasswordMigrationService {
 
-    private final UserReposetries userReposetries;
-    private final PasswordEncoder passwordEncoder;
+        private final UserReposetries userReposetries;
+        private final PasswordEncoder passwordEncoder;
 
-    public PasswordMigrationService(UserReposetries userReposetries, PasswordEncoder passwordEncoder) {
-        this.userReposetries = userReposetries;
-        this.passwordEncoder = passwordEncoder;
-    }
+        public PasswordMigrationService(UserReposetries userReposetries, PasswordEncoder passwordEncoder) {
+            this.userReposetries = userReposetries;
+            this.passwordEncoder = passwordEncoder;
+        }
 
-    public void migratePasswords() {
-        Iterable<User> users = userReposetries.findAll();
+        public void migratePasswords() {
+            Iterable<User> users = userReposetries.findAll();
 
-        for (User user : users) {
-            String password = user.getPassword();
+            for (User user : users) {
+                String password = user.getPassword();
 
-            if (password == null || password.isEmpty()) continue;
+                if (password == null || password.isEmpty()) continue;
 
-            if (password.startsWith("$2a$") || password.startsWith("$2b$") || password.startsWith("$2y$")) {
-                continue;
+                if (password.startsWith("$2a$") || password.startsWith("$2b$") || password.startsWith("$2y$")) {
+                    continue;
+                }
+                String encodedPassword = passwordEncoder.encode(password);
+                user.setPassword(encodedPassword);
+                userReposetries.save(user);
             }
-            String encodedPassword = passwordEncoder.encode(password);
-            user.setPassword(encodedPassword);
-            userReposetries.save(user);
         }
     }
-}
--- src/test/java/org/orange/oie/internship2025/assetmanagementsystem/AuthControllerIntegrationTest.java
+++ src/test/java/org/orange/oie/internship2025/assetmanagementsystem/AuthControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.orange.oie.internship2025.assetmanagementsystem.entity.User;
 import org.orange.oie.internship2025.assetmanagementsystem.reposetries.DepartmentReposetries;
 import org.orange.oie.internship2025.assetmanagementsystem.reposetries.RoleReposetries;
 import org.orange.oie.internship2025.assetmanagementsystem.reposetries.UserReposetries;
+import org.orange.oie.internship2025.assetmanagementsystem.serivce.PasswordMigrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -40,15 +41,13 @@ public class AuthControllerIntegrationTest {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    PasswordMigrationService p;
     @BeforeEach
     void setUp() {
         userReposetries.deleteAll();
         departmentReposetries.deleteAll();
         roleReposetries.deleteAll();
-    }
-
-    @Test
-    void shouldReturnOkAndUserDTOWhenAuthenticationIsSuccessful() throws Exception {
         Department department = new Department();
         department.setDepartmentName("IT");
         departmentReposetries.save(department);
@@ -59,11 +58,17 @@ public class AuthControllerIntegrationTest {
 
         User user = new User();
         user.setEmail("test@orange.com");
-        user.setPassword(passwordEncoder.encode("correct-password"));
+        user.setPassword(("correct-password"));
         user.setUsername("testuser");
         user.setRole(role);
         user.setDepartment(department);
         userReposetries.save(user);
+        p.migratePasswords();
+    }
+
+    @Test
+    void shouldReturnOkAndUserDTOWhenAuthenticationIsSuccessful() throws Exception {
+
 
         String credentials = "test@orange.com:correct-password";
         String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes());
@@ -77,21 +82,7 @@ public class AuthControllerIntegrationTest {
 
     @Test
     void shouldReturnUnauthorizedWhenAuthenticationFails() throws Exception {
-        Department department = new Department();
-        department.setDepartmentName("IT");
-        departmentReposetries.save(department);
 
-        Role role = new Role();
-        role.setRoleType("Employee");
-        roleReposetries.save(role);
-
-        User user = new User();
-        user.setEmail("test@orange.com");
-        user.setPassword(passwordEncoder.encode("correct-password"));
-        user.setUsername("testuser");
-        user.setRole(role);
-        user.setDepartment(department);
-        userReposetries.save(user);
 
         String badCredentials = "test@orange.com:wrong-password";
         String encodedBadCredentials = Base64.getEncoder().encodeToString(badCredentials.getBytes());
