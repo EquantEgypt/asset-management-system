97f6f6f3d333a9bc962a6cc2d8059abd677c3727
Zeyad <farghlyzeyad9@gmail.com>
2025-09-09 19:00:05 +0300

    add controllers for types and categories

---
+++ src/main/java/org/orange/oie/internship2025/assetmanagementsystem/controller/TypeController.java
@@ -0,0 +1,25 @@
+package org.orange.oie.internship2025.assetmanagementsystem.controller;
+
+import org.orange.oie.internship2025.assetmanagementsystem.entity.Type;
+import org.orange.oie.internship2025.assetmanagementsystem.repository.TypeRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class TypeController {
+
+    private final TypeRepository typeRepository;
+
+    public TypeController(TypeRepository typeRepository) {
+        this.typeRepository = typeRepository;
+    }
+
+    @GetMapping("/types")
+    public List<Type> getAllTypes() {
+        return typeRepository.findAll();
+    }
+}
