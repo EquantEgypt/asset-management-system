f25524f05a333ab8c4ca776f7abc0bc3d38e465c
Malak Ziad <malekziad81@gmail.com>
2025-09-09 12:02:39 +0300

    updated list of users admin_users only

--- src/main/java/org/orange/oie/internship2025/assetmanagementsystem/dto/UserDTO.java
+++ src/main/java/org/orange/oie/internship2025/assetmanagementsystem/dto/UserDTO.java
@@ -14,7 +14,7 @@ public class UserDTO {
     private Long id;
     private String username;
     private String email;
-    private String role; // mapped to RoleType enum in DB
+    private Role role; // mapped to RoleType enum in DB
     private String departmentName;  // read-friendly instead of just ID
-    private Long departmentId;
+private Long departmentId;
 }
