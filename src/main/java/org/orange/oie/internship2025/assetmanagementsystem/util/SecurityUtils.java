f0ea7179590c4ec90d98eb71fd26888485b53eca
Ahmed-Eldera <hotoeldera@yahoo.com>
2025-09-09 11:01:20 +0300

    added SecurityUtil to use getCurrentLoggedinUser

---
+++ src/main/java/org/orange/oie/internship2025/assetmanagementsystem/util/SecurityUtils.java
@@ -0,0 +1,44 @@
+package org.orange.oie.internship2025.assetmanagementsystem.util;
+
+import org.orange.oie.internship2025.assetmanagementsystem.entity.User;
+import org.orange.oie.internship2025.assetmanagementsystem.security.CustomUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public final class SecurityUtils {
+
+    private SecurityUtils() {
+
+    }
+
+    public static User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetails)) {
+            return null;
+        }
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        return userDetails.getUser();
+    }
+
+    public static String getCurrentUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetails)) {
+            return null;
+        }
+
+        return ((CustomUserDetails) authentication.getPrincipal()).getUsername();
+    }
+
+    public static long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetails)) {
+            return -1;
+        }
+
+        return ((CustomUserDetails) authentication.getPrincipal()).getUser().getUserId();
+    }
+}
