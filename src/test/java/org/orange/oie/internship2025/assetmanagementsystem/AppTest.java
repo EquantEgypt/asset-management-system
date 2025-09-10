17c90b043196bcb328c02e5abebe9cb03c34775f
Zeyad <farghlyzeyad9@gmail.com>
2025-09-10 14:51:02 +0300

    testing

--- src/test/java/org/orange/oie/internship2025/assetmanagementsystem/AppTest.java
+++ src/test/java/org/orange/oie/internship2025/assetmanagementsystem/AppTest.java
@@ -2,17 +2,14 @@ package org.orange.oie.internship2025.assetmanagementsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AppTest {
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    void main() {
-        App.main(new String[]{});
     }
 
 }
