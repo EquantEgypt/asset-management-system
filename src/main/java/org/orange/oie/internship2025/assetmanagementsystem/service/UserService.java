b07feafdb6d199e355e48431ce31d5c805f54edd
Maryiamreda <maryiamreda@gmail.com>
2025-09-10 18:38:30 +0200

    search for user by name Specification

--- src/main/java/org/orange/oie/internship2025/assetmanagementsystem/service/UserService.java
+++ src/main/java/org/orange/oie/internship2025/assetmanagementsystem/service/UserService.java
@@ -5,9 +5,11 @@ import org.orange.oie.internship2025.assetmanagementsystem.dto.UserDTO;
 import org.orange.oie.internship2025.assetmanagementsystem.entity.User;
 import org.orange.oie.internship2025.assetmanagementsystem.mapper.UserMapper;
 import org.orange.oie.internship2025.assetmanagementsystem.repository.UserRepository;
+import org.orange.oie.internship2025.assetmanagementsystem.specification.UserSpecifications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,14 +19,16 @@ public class UserService {
     
 
     public Page<UserDTO> getAllUsers(Pageable pageable) {
-        Page<User> users = userRepository.findAll(pageable); // Use pageable parameter
-        return UserMapper.toDtoPage(users); // Use the new method for Page conversion
+        Page<User> users = userRepository.findAll(pageable);
+        return UserMapper.toDtoPage(users);
     }
     public Page<UserDTO> getUserByDepartment(Long  department, Pageable pageable) {
         Page<User> usersbydep =userRepository.findByDepartment_DepartmentId(department , pageable);
         return UserMapper.toDtoPage(usersbydep);
-
     }
-
+    public Page<UserDTO> getusersByName(String username, Pageable pageable) {
+        Page<User> users = userRepository.findAll(UserSpecifications.hasName(username), pageable);
+        return UserMapper.toDtoPage(users);
+    }
 
 }
