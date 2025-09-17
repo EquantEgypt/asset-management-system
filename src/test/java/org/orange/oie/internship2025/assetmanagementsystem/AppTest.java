93c4d6ce1da8ba3833dcc19ca241c5f41d025e4d
Zeyad <farghlyzeyad9@gmail.com>
2025-09-17 21:24:00 +0300

    auth test

--- src/test/java/org/orange/oie/internship2025/assetmanagementsystem/AppTest.java
+++ src/test/java/org/orange/oie/internship2025/assetmanagementsystem/AppTest.java
@@ -1,15 +1,15 @@
-//package org.orange.oie.internship2025.assetmanagementsystem;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//@SpringBootTest
-//@ActiveProfiles("test")
-//class AppTest {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package org.orange.oie.internship2025.assetmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
