ff1a03a6c67ff1c7ef8773238a2f4337a769b5e0
Ahmed-Eldera <hotoeldera@yahoo.com>
2025-09-17 14:27:52 +0300

    dbInit bean intial implementation

--- src/main/java/org/orange/oie/internship2025/assetmanagementsystem/repository/CategoryRepository.java
+++ src/main/java/org/orange/oie/internship2025/assetmanagementsystem/repository/CategoryRepository.java
@@ -3,4 +3,5 @@ import org.orange.oie.internship2025.assetmanagementsystem.entity.AssetCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<AssetCategory, Long> {
+    AssetCategory findByName(String name);
 }
