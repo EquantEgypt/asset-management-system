07fec7e17474f6b2b839b11aebd1ae46e6641a8c
Ahmed-Eldera <hotoeldera@yahoo.com>
2025-09-19 18:26:34 +0300

    intial api working
    # Conflicts:
    #	src/main/java/org/orange/oie/internship2025/assetmanagementsystem/entity/AssetRequest.java
    #	src/main/resources/db/migration/V1__create_ams_tables.sql

--- src/main/java/org/orange/oie/internship2025/assetmanagementsystem/controller/RequestController.java
+++ src/main/java/org/orange/oie/internship2025/assetmanagementsystem/controller/RequestController.java
@@ -2,8 +2,9 @@ package org.orange.oie.internship2025.assetmanagementsystem.controller;
 
 
 import jakarta.validation.Valid;
-import org.orange.oie.internship2025.assetmanagementsystem.dto.RequestDto;
-import org.orange.oie.internship2025.assetmanagementsystem.dto.ResponseDto;
+import org.orange.oie.internship2025.assetmanagementsystem.dto.RequestDTO;
+import org.orange.oie.internship2025.assetmanagementsystem.dto.ResponseDTO;
+import org.orange.oie.internship2025.assetmanagementsystem.service.serviceInterface.RequestService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/request")
 public class RequestController {
-
+    private final RequestService requestService;
+    public RequestController(RequestService requestService){
+        this.requestService = requestService;
+    }
 
     @PostMapping
-    public ResponseEntity<ResponseDto> addRequest(@Valid @RequestBody RequestDto requestDto) {
-
-        return ResponseEntity.ok(null);
+    public ResponseEntity<ResponseDTO> addRequest(@Valid @RequestBody RequestDTO requestDTO) {
+       ResponseDTO response =  requestService.addRequest(requestDTO);
+        return ResponseEntity.ok(response);
 
     }
 
