68d94dae6fc69ccfc2fcd1c921c41fa5a0b0c6fd
Ahmed-Eldera <hotoeldera@yahoo.com>
2025-09-19 18:26:34 +0300

    intial api working
    # Conflicts:
    #	src/main/java/org/orange/oie/internship2025/assetmanagementsystem/entity/AssetRequest.java
    #	src/main/resources/db/migration/V1__create_ams_tables.sql

--- src/main/java/org/orange/oie/internship2025/assetmanagementsystem/service/serviceInterface/RequestService.java
+++ src/main/java/org/orange/oie/internship2025/assetmanagementsystem/service/serviceInterface/RequestService.java
@@ -1,4 +1,8 @@
 package org.orange.oie.internship2025.assetmanagementsystem.service.serviceInterface;
+import org.orange.oie.internship2025.assetmanagementsystem.dto.RequestDTO;
+import org.orange.oie.internship2025.assetmanagementsystem.dto.ResponseDTO;
 
 public interface RequestService {
+
+     ResponseDTO addRequest(RequestDTO requestDTO);
 }
