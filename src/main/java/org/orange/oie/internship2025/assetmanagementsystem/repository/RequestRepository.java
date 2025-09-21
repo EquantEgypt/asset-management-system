474185f5df7410961b647c8e492e16de48969cfb
Zeyad <farghlyzeyad9@gmail.com>
2025-09-21 15:14:37 +0300

    demo

--- src/main/java/org/orange/oie/internship2025/assetmanagementsystem/repository/RequestRepository.java
+++ src/main/java/org/orange/oie/internship2025/assetmanagementsystem/repository/RequestRepository.java
@@ -1,9 +1,17 @@
 package org.orange.oie.internship2025.assetmanagementsystem.repository;
 
 import org.orange.oie.internship2025.assetmanagementsystem.entity.AssetRequest;
+import org.orange.oie.internship2025.assetmanagementsystem.enums.RequestType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RequestRepository extends JpaRepository<AssetRequest, Long> {
+    Page<AssetRequest> findAllByRequesterIdIn(List<Long> requesterIds, Pageable pageable);
+    Page<AssetRequest> findAllByRequesterId(Long requesterId, Pageable pageable);
+    Page<AssetRequest> findAllByRequestType(RequestType requestType, Pageable pageable);
 }
