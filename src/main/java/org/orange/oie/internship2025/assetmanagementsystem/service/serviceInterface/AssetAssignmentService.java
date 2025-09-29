4ddf04d42fa22d2451fe91ea01df1caf2a8bbb00
Ahmed-Eldera <hotoeldera@yahoo.com>
2025-09-30 00:13:46 +0300

    intial api without testing

--- src/main/java/org/orange/oie/internship2025/assetmanagementsystem/service/serviceInterface/AssetAssignmentService.java
+++ src/main/java/org/orange/oie/internship2025/assetmanagementsystem/service/serviceInterface/AssetAssignmentService.java
@@ -1,6 +1,7 @@
 package org.orange.oie.internship2025.assetmanagementsystem.service.serviceInterface;
 
 import org.orange.oie.internship2025.assetmanagementsystem.dto.AssetAssignmentRequest;
+import org.orange.oie.internship2025.assetmanagementsystem.dto.AssetDto;
 import org.orange.oie.internship2025.assetmanagementsystem.entity.AssetAssignment;
 import org.orange.oie.internship2025.assetmanagementsystem.entity.AssetCategory;
 import org.orange.oie.internship2025.assetmanagementsystem.entity.AssetType;
@@ -8,6 +9,6 @@ import org.orange.oie.internship2025.assetmanagementsystem.errors.ApiResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface AssetAssignmentService {
-
     AssetAssignment assignAsset(AssetAssignmentRequest request);
+    AssetDto unassignAsset(Long id);
 }
