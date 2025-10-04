b10157af6861302847b2c2f81250bac5bf4558aa
Zeyad <farghlyzeyad9@gmail.com>
2025-10-04 20:38:57 +0300

    resolve conflicts

--- src/main/java/org/orange/oie/internship2025/assetmanagementsystem/mapper/AssetMapper.java
+++ src/main/java/org/orange/oie/internship2025/assetmanagementsystem/mapper/AssetMapper.java
@@ -127,30 +127,4 @@ public class AssetMapper {
         return dto;
     }
 
-    public AssetDetailsDto toDetailsDto(Asset asset) {
-        AssetDetailsDto dto = new AssetDetailsDto();
-        dto.setAssetId(asset.getId());
-        dto.setAssetName(asset.getName());
-        dto.setBrand(asset.getBrand());
-        dto.setAssetDescription(asset.getDescription());
-        dto.setCategoryName(asset.getCategory().getName());
-        dto.setTypeName(asset.getType().getName());
-        dto.setLocation(asset.getLocation());
-        dto.setSerialNumber(asset.getSerialNumber());
-        dto.setPurchaseDate(asset.getPurchaseDate().toString());
-        dto.setWarrantyEndDate(asset.getWarrantyEndDate().toString());
-        dto.setStatus(asset.getStatus());
-        dto.setImagePath(asset.getImagePath());
-
-        if (asset.getAssignments() != null && !asset.getAssignments().isEmpty()) {
-            var userAssignment = asset.getAssignments().get(asset.getAssignments().size() - 1);
-            User assignedUser = userAssignment.getAssignedTo();
-            dto.setAssignedToId(assignedUser.getId());
-            dto.setAssignedToName(assignedUser.getUsername());
-            dto.setAssignedToEmail(assignedUser.getEmail());
-        }
-
-        return dto;
-    }
-
 }
